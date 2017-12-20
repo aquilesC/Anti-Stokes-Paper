880baddf3a351aba532d8fd554d72921b111c1ac
Martin Caldarola <caldarola@physics.leidenuniv.nl>
2017-12-20 12:08:31 +0100

    made some small changes to the figure and important changes to the text to avoid repetition. corrected the references to the suppp info and put it in the correct order. Rechecked the error for the temperature values in figure 4 and put the correct errorbars in the plot in the supp material. added the citation Orrit Kottis, still need to find it.

--- Figures/03_Fit_Of_AS/analysis/temperature.m
+++ Figures/03_Fit_Of_AS/analysis/temperature.m
@@ -27,12 +27,7 @@ ylim([380 500])
 t1 = T1-mean(T1);
 t2 = T2-mean(T2);
 t3 = T3-mean(T3);
-% 
-% figure
-% plot(I1,t1,'o')
-% hold all
-% plot(I2,t2,'s')
-% plot(I3,t3,'x')
+
 %% fit
 I = vertcat(I1,I2,I3);
 T = vertcat(t1,t2,t3);
@@ -40,8 +35,8 @@ T = vertcat(t1,t2,t3);
 T = T(ind);
 clear ind
 
-[p,S] = polyfit(I,T,1)
-Rinv = inv(S.R)
+[p,S] = polyfit(I,T,1);
+Rinv = inv(S.R);
 C = (Rinv*Rinv')*S.normr^2/S.df
 
 [y,delta] = polyval(p,0,S)
@@ -79,30 +74,39 @@ figure(3)
 clf
 set(gcf,'units','centimeters')
 set(gcf,'position',[10 20 85*3 55*3]/10)
-
-plot(I1,T1,'or','color',[255 165 0]/255,'linewidth',2,'markersize',ms)
-hold all
-plot(I2,T2,'x','color',[0 204 0]/255,'linewidth',2,'markersize',ms)
-plot(I3,T3,'sr','color',[0 0 204]/255,'linewidth',2,'markersize',ms)
-ylim([380 500])
+% plot the fits
 plot(x,polyval(p1,x),'--','color',[255 165 0]/255,'linewidth',2)
+hold all
 plot(x,polyval(p2,x),'--','color',[0 204 0]/255,'linewidth',2)
 plot(x,polyval(p3,x),'--','color',[0 0 204]/255,'linewidth',2)
+
+errores = true;
+if errores== false
+    hh1 = plot(I1,T1,'or','color',[255 165 0]/255,'linewidth',2,'markersize',ms,'markerfacecolor','w');
+    hh2 = plot(I2,T2,'^','color',[0 204 0]/255,'linewidth',2,'markersize',ms,'markerfacecolor','w');
+    hh3 = plot(I3,T3,'sr','color',[0 0 204]/255,'linewidth',2,'markersize',ms,'markerfacecolor','w');
+else
+    hh1 = errorbar(I1,T1,ones(size(I1)).*5,'or','color',[255 165 0]/255,'linewidth',2,'markersize',ms,'markerfacecolor','w');
+    hh2 = errorbar(I2,T2,ones(size(I2)).*5,'^','color',[0 204 0]/255,'linewidth',2,'markersize',ms,'markerfacecolor','w');
+    hh3 = errorbar(I3,T3,ones(size(I3)).*5,'sr','color',[0 0 204]/255,'linewidth',2,'markersize',ms,'markerfacecolor','w');
+end
+ylim([380 500])
+
 % xlabel('Laser power [\muW]','FontSize',20)
 % ylabel('Temperature [T]','FontSize',20)
 set(gca,'linewidth',3)
 set(gca,'fontsize',16)
-lll=legend('20 ^oC','40 ^oC','60 ^oC');
+lll=legend([hh1 hh2 hh3],{'20 ^oC','40 ^oC','60 ^oC'});
 set(lll,'location','northwest')
 
-saveas(gcf,'TvsInt','fig')
-saveas(gcf,'TvsInt','pdf')
+% saveas(gcf,'TvsInt','fig')
+% saveas(gcf,'TvsInt','pdf')
 
 
 %% 
 Treal = [293 313 333];
 Tmea = [p1(2) p2(2) p3(2)];
-sTmea = 7*ones(size(Tmea));
+sTmea = 8*ones(size(Tmea));
 
 
 
@@ -111,22 +115,22 @@ figure(4)
 set(gcf,'units','centimeters')
 set(gcf,'position',[10 20 25*5 18*5]/10)
 clf
-errorbar(Treal,Tmea,sTmea,'s','linewidth',2,'color',[0 0 204]/255,'markersize',ms,'MarkerEdgeColor',[0 0 204]/255,...
+errorbar(Treal,Tmea,sTmea/2,'s','linewidth',2,'color',[0 0 204]/255,'markersize',ms,'MarkerEdgeColor',[0 0 204]/255,...
     'MarkerFaceColor',[0 0 204]/255)
 hold all
 plot(v,v,'r','linewidth',2)
 xlim(v)
 ylim([280 360])
-
+plot(v,v+ori,'--','linewidth',2')
 % xlabel('Temperature flow cell [T]','FontSize',20)
 % ylabel('Extracted Temperature [T]','FontSize',20)
 set(gca,'linewidth',3)
 set(gca,'fontsize',16)
-xticks(290:15:345)
-yticks(280:15:360)
+xticks(280:20:340)
+yticks(280:20:360)
 
-saveas(gcf,'Inset','fig')
-saveas(gcf,'Inset','pdf')
+% saveas(gcf,'Inset','fig')
+% saveas(gcf,'Inset','pdf')
 %% from aquiles
 p1a =[0.71225756  298.08501504];
 p2a =[0.71225756  316.62098401];
@@ -134,20 +138,20 @@ p3a =[0.71225756  340.344689644];
 % Tmea_a = [298.08501504  316.62098401  340.34468964]; %witout specifiy tol
 % with tol 1e-3 in the jupyter program
 Tmea_a = [296  315  339]
-sTmea_a = [4 4 4];
-ori = mean(Tmea_a-Treal)
+sTmea_a = 6*ones(size(Tmea_a));
+ori = mean(Tmea_a-Treal);
 
 Treal
 
-figure(4)
+figure(5)
 set(gcf,'units','centimeters')
 set(gcf,'position',[10 20 25*5 18*5]/10)
 clf
-plot(v,v+ori,'--','linewidth',2')
+plot(v,v+ori,'--','linewidth',2','color',[0 0 204]/255)
 hold all
 plot(v,v,'r','linewidth',2)
-errorbar(Treal,Tmea_a,sTmea_a,'s','linewidth',2,'color',[0 0 204]/255,'markersize',ms,'MarkerEdgeColor',[0 0 204]/255,...
-    'MarkerFaceColor',[0 0 204]/255)
+errorbar(Treal,Tmea_a,sTmea_a/2,'s','linewidth',2,'color',[0 0 204]/255,'markersize',ms,'MarkerEdgeColor',[0 0 204]/255,...
+    'MarkerFaceColor','w')
 
 
 
